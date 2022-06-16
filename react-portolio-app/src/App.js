18b3e56c7e3da1568f1a74fe6e8cae14d5240429
Shameem <shameem.abdulkarim@gmail.com>
2022-06-16 17:23:55 +0200

    styled aboutsection

--- react-portolio-app/src/App.js
+++ react-portolio-app/src/App.js
@@ -1,8 +1,11 @@
 import AboutUs from "./pages/AboutUs";
+//Global style
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
