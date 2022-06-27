d68da302b9c2167c0a3669a039ead40d162ca950
Shameem <shameem.abdulkarim@gmail.com>
2022-06-27 23:25:08 +0200

    ourwork and faq toggle

--- react-portolio-app/src/components/FaqSection.js
+++ react-portolio-app/src/components/FaqSection.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const Faqsection = () => {
   return (
@@ -8,58 +10,52 @@ const Faqsection = () => {
       <h2>
         Any questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start ?</h4>
-        <div className="answer">
-          <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
-            adipisci magni aut commodi reiciendis tempore id deleniti
-            aspernatur, quibusdam accusantium illo nesciunt quisquam repudiandae
-            quidem incidunt molestiae explicabo deserunt dolore!
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
-            adipisci magni aut commodi reiciendis tempore id deleniti
-            aspernatur, quibusdam accusantium illo nesciunt quisquam repudiandae
-            quidem incidunt molestiae explicabo deserunt dolore!
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
-            adipisci magni aut commodi reiciendis tempore id deleniti
-            aspernatur, quibusdam accusantium illo nesciunt quisquam repudiandae
-            quidem incidunt molestiae explicabo deserunt dolore!
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What product do you offer ?</h4>
-        <div className="answer">
-          <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
-            adipisci magni aut commodi reiciendis tempore id deleniti
-            aspernatur, quibusdam accusantium illo nesciunt quisquam repudiandae
-            quidem incidunt molestiae explicabo deserunt dolore!
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start ?">
+          <div className="answer">
+            <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              adipisci magni aut commodi reiciendis tempore id deleniti
+              aspernatur, quibusdam accusantium illo nesciunt quisquam
+              repudiandae quidem incidunt molestiae explicabo deserunt dolore!
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              adipisci magni aut commodi reiciendis tempore id deleniti
+              aspernatur, quibusdam accusantium illo nesciunt quisquam
+              repudiandae quidem incidunt molestiae explicabo deserunt dolore!
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              adipisci magni aut commodi reiciendis tempore id deleniti
+              aspernatur, quibusdam accusantium illo nesciunt quisquam
+              repudiandae quidem incidunt molestiae explicabo deserunt dolore!
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What product do you offer ?">
+          <div className="answer">
+            <p>lorem ipsumsfsgsdfgsdf sdfgsfg</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam
+              adipisci magni aut commodi reiciendis tempore id deleniti
+              aspernatur, quibusdam accusantium illo nesciunt quisquam
+              repudiandae quidem incidunt molestiae explicabo deserunt dolore!
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
